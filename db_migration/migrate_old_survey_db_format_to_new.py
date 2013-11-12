c4e5c482652b4131ab893e042d5873f85e50a1d8
Matthew Cox <matthew.cox@teachforamerica.org>
2013-11-12 16:32:11 -0600

    Added migration script

---
+++ db_migration/migrate_old_survey_db_format_to_new.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+#Read data
+responses = pd.read_csv('numerical_responses.csv')
+question_table = pd.read_csv('survey_specific_questions.csv')
+
+#Create survey table data
+survey_codes = question_table.survey.unique().tolist()
+survey_map = dict(zip(survey_codes,range(len(survey_codes))))
+survey_ids = [survey_map[survey_name] for survey_name in survey_codes]
+
+pd.DataFrame({'survey_id':survey_ids,'survey_code':survey_codes}).to_csv('surveys.csv')
+
+#Create question table data
+questions = question_table.survey_specific_qid.unique().tolist()
+question_map = dict(zip(questions,range(len(questions))))
+
+question_table['question_id'] = question_table.survey_specific_qid.map(question_map)
+question_table['survey_id'] = question_table.survey.map(survey_map)
+
+pd.DataFrame(question_table,columns=['question_id','survey_id','master_qid']).to_csv('questions.csv')
+
+#Map survey id and question id onto survey responses
+responses['question_id'] = responses.survey_specific_qid.map(question_map)
+responses['survey_id'] = responses.survey.map(survey_map)
+responses = responses.rename(columns={'cm_pid':'respondent_id'})
+
+#Create results table
+pd.DataFrame(responses,columns=['respondent_id','survey_id','question_id','response']).to_csv('responses.csv')
