a4c83aebbd112060046051bda69efbaed1251243
Matthew Cox <matthew.cox@teachforamerica.org>
2014-01-09 17:22:26 -0600

    Modify net values generation for list of question types

--- database/mdis_db_migration/migrate_old_survey_db_format_to_new.py
+++ database/mdis_db_migration/migrate_old_survey_db_format_to_new.py
@@ -4,7 +4,7 @@ from sqlalchemy.engine import reflection
 import numpy as np
 from alembic.migration import MigrationContext
 from alembic.operations import Operations
-
+import pdb
 
 
 #Connect to local DB
@@ -19,7 +19,8 @@ survey_specific_questions = Table('survey_specific_questions',metadata,
 							Column('survey_specific_qid',String),
 							Column('master_qid',String),
 							Column('survey',String),
-							Column('confidential',Integer)
+							Column('confidential',Integer),
+							Column('question_type',String)
 							)
 engine_1 = create_engine('mysql+oursql://mcox:fa1c0n@localhost/mdis_survey_database')
 conn_1 = engine_1.connect()
@@ -37,7 +38,8 @@ questions = Table('questions',metadata,
 			Column('id', Integer, primary_key=True),
 			Column('survey_id', Integer),
 			Column('question_code', String(20)),
-			Column('is_confidential', Integer))
+			Column('is_confidential', Integer),
+			Column('question_type', String(20)),)
 
 connect_info_file = open('db_connect_string.txt')
 connect_info = connect_info_file.readline()
@@ -92,7 +94,13 @@ question_map = dict(zip(questions_list,range(len(questions_list))))
 question_table['question_id'] = question_table.survey_specific_qid.map(question_map)
 question_table['survey_id'] = question_table.survey.map(survey_map)
 question_table = question_table.rename(columns={'master_qid':'question_code','question_id':'id','confidential':'is_confidential'})
-questions_for_db = pd.DataFrame(question_table,columns=['id','survey_id','question_code','is_confidential'])
+questions_for_db = pd.DataFrame(question_table,columns=['id','survey_id','question_code','is_confidential','question_type'])
+
+#Change questions
+# pdb.set_trace()
+questions_for_db.ix[questions_for_db.question_type == '7pt_Net_1=SA','question_type'] = '7pt_1=SA'
+questions_for_db.ix[questions_for_db.question_type == '7pt_NCS_1=SA','question_type'] = '7pt_1=SA'
+questions_for_db.ix[questions_for_db.question_type == '7pt_NCS_7=SA','question_type'] = '7pt_7=SA'
 
 #Map survey id and question id onto survey responses
 responses['question_id'] = responses.survey_specific_qid.map(question_map)
