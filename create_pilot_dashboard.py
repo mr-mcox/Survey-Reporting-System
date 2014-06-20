d0c495f1305705f8387e3c77884e30300ebd4683
Matthew Cox <matthew.cox@teachforamerica.org>
2014-06-20 09:35:28 -0500

    Modifying pilot cms to pull directly from the database

--- create_pilot_dashboard.py
+++ create_pilot_dashboard.py
@@ -7,28 +7,23 @@ import logging
 import os
 import csv
 import pdb
+import sys
 
 logging.basicConfig(filename='debug.log',level=logging.WARNING)
 
 config = ConfigurationReader.ConfigurationReader(config_file='config.yaml')
 
-# Read connection info
-# connect_info_file = open('db_connect_string.txt')
-# connect_info = connect_info_file.readline()
-# connect_info_file.close()
+connect_info = 'mssql+pyodbc://survey_user:Survey!1@tpsd_survey'
 
-# engine = create_engine(connect_info)
+engine = create_engine(connect_info)
 
-# conn = engine.connect()
-# db = conn
-# retriever = ResultsRetriever.ResultsRetriever(db_connection=db)
-# print("Starting to retrieve results")
-# results = retriever.retrieve_results_for_survey(survey_code='1314F8W')
-# results_df = pd.DataFrame(results['rows'])
-# results_df.columns = results['column_headings']
-
-# results_df.to_csv('results.csv')
-results_df = pd.read_csv('results.csv')
+conn = engine.connect()
+db = conn
+retriever = ResultsRetriever.ResultsRetriever(db_connection=db)
+print("Starting to retrieve current results")
+results = retriever.retrieve_results_for_survey(survey_code=sys.argv[1])
+results_df = pd.DataFrame(results['rows'])
+results_df.columns = results['column_headings']
 
 for_historical = False	
 print("Starting calculations")
