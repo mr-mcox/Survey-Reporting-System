8a1cef3b976bc105c0e40e89b89d1ad73289dbea
Matthew Cox <matthew.cox@teachforamerica.org>
2014-10-07 16:36:46 -0500

    Setting up migration script

---
+++ db_migration/migrate.py
@@ -0,0 +1,102 @@
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, select
+
+class migrate(object):
+	"""docstring for migrate"""
+	def __init__(self, connection):
+		
+
+		metadata = MetaData()
+		#Tables
+		numerical_responses = Table('numerical_responses',metadata,
+								Column('cm_pid', Integer),
+								Column('survey', String),
+								Column('survey_specific_qid', String),
+								Column('response', Integer))
+
+		survey_specific_questions = Table('survey_specific_questions',metadata,
+								Column('survey_specific_qid',String),
+								Column('master_qid',String),
+								Column('survey',String),
+								Column('confidential',Integer),
+								Column('question_type',String)
+								)
+		question = Table('cm_question',metadata,
+			Column('question_id', Integer, primary_key=True, autoincrement=False),
+			Column('question_title', String(2000)),
+			Column('question_code', String(20)),
+			)
+		question_category = Table('cm_question_category',metadata,
+			Column('question_category_id', Integer, primary_key=True, autoincrement=False),
+			Column('question_category', String(20)),
+			)
+		response = Table('cm_response',metadata,
+			Column('person_id', Integer, primary_key=True, autoincrement=False),
+			Column('survey_question_id', Integer, primary_key=True, autoincrement=False),
+			Column('response', Integer),
+			Column('converted_net_value', Integer),
+			)
+		survey = Table('cm_survey',metadata,
+			Column('survey_id', Integer, primary_key=True, autoincrement=False),
+			Column('survey_code', String(20)),
+			Column('survey_title', String(2000)),
+			)
+		survey_question = Table('cm_survey_question',metadata,
+			Column('survey_question_id', Integer, primary_key=True, autoincrement=False),
+			Column('survey_id', Integer),
+			Column('is_confidential', Integer),
+			Column('question_type', String(20)),
+			Column('question_title_override', String(2000)),
+			Column('question_id', Integer),
+			Column('question_category_id', Integer),
+			)
+		self.table = {
+			'numerical_responses':numerical_responses,
+			'survey_specific_questions':survey_specific_questions,
+			'question':question,
+			'question_category':question_category,
+			'response':response,
+			'survey':survey,
+			'survey_question':survey_question,
+		}
+
+#Create survey question category map based on question code and asign arbitrary ids
+
+#Create map of question code to question_category_id
+
+#Extract survey code and title from survey_specific_questions
+
+#Write survey title
+
+#Assign survey_id and map to survey code
+
+#Extract question code, title, confidential, survey_specific_qid and survey from survey_specific_questions
+
+#Create question_code_survey column, survey_question_id and map
+
+#Pick most recent question title based on most recent survey
+
+#Create question_id and map
+
+#Map question_category_id on survey_question
+
+#Map survey_id on survey_question
+
+#Map question_id on survey_question
+
+#Create question_override title if survey question doesn't match with associated question
+
+#Extract survey_specific_qid, cm_pid, repsonse from numeric_responses
+
+#Map responses based on survey_specific_qid
+
+#Map converted_net_value based on response
+
+#Import survey
+
+#Import survey_question_category
+
+#Import survey_question
+
+#Import question
+
+#Import cm_survey_response
