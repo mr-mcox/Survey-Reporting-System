56a6e3f81c82c15da85d1479d50d918e29630886
Matthew Cox <matthew.cox@teachforamerica.org>
2014-10-07 23:08:51 -0500

    Import cm_survey_response

--- db_migration/migrate.py
+++ db_migration/migrate.py
@@ -34,9 +34,8 @@ class Migrator(object):
             )
         response = Table('cm_response',metadata,
             Column('person_id', Integer, primary_key=True, autoincrement=False),
-            Column('survey_question_id', Integer, primary_key=True, autoincrement=False),
-            Column('response', Integer),
-            Column('converted_net_value', Integer),
+
+
             )
         survey = Table('cm_survey',metadata,
             Column('survey_id', Integer, primary_key=True, autoincrement=False),
@@ -234,3 +233,4 @@ class Migrator(object):
 
     def migrate_to_new_schema(self):
         self.survey_df.ix[:,['survey_id','survey_code','survey_title']].to_sql('cm_survey',self.engine,index=False,if_exists='append')
+        self.response_df.ix[:,['person_id','survey_question_id','response','converted_net_value']].to_sql('cm_response',self.engine,index=False,if_exists='append')
--- db_migration/test/test_db_migration.py
+++ db_migration/test/test_db_migration.py
@@ -293,3 +293,10 @@ def test_migrate_survey_table_to_new_schema(migrator_with_ssq_and_nr_for_respons
 	records = m.db.execute(select([m.table['survey']]))
 	table_df = pd.DataFrame.from_records(records.fetchall(),columns=records.keys())
 	pd.util.testing.assert_frame_equal(pd.DataFrame(m.survey_df,columns=records.keys()), table_df)
+
+def test_migrate_response_table_to_new_schema(migrator_with_ssq_and_nr_for_response):
+	m = migrator_with_ssq_and_nr_for_response
+	m.migrate_to_new_schema()
+	records = m.db.execute(select([m.table['response']]))
+	table_df = pd.DataFrame.from_records(records.fetchall(),columns=records.keys())
+	pd.util.testing.assert_frame_equal(pd.DataFrame(m.response_df,columns=records.keys()), table_df)
